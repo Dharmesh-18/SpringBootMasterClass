6fcf753fdfc837e2d1d1bea440cda681e2a9fa8f
WARBREAKER <dharmeshtiwari1280@gmail.com>
2023-05-24 21:29:31 +0530

    Jackson JsonProperty and JsonIgnore

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -1,21 +1,33 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
 
     private final Long id;
     private final String name;
+    private final String password;
 
-    public Customer(Long id, String name) {
+    public Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonProperty("customer_id")
     public Long getId() {
         return id;
     }
 
+//    @JsonProperty("customer_name")
     public String getName() {
         return name;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
     }
 
     @Override
--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -9,8 +9,8 @@ public class CustomerFakeRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomer() {
         return Arrays.asList(
-                new Customer(1L, "James Bond"),
-                new Customer(2L, "Black Panther")
+                new Customer(1L, "James Bond", "password123"),
+                new Customer(2L, "Black Panther", "password12345")
         );
     }
 }
