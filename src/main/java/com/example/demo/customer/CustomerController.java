f200171bf6bef055308211e92ead603ad4626df8
WARBREAKER <dharmeshtiwari1280@gmail.com>
2023-05-24 19:41:34 +0530

    Implementing to interface, using qualifier in case of multiple implementations of same Interface(bean)

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -5,6 +5,8 @@ import com.example.demo.DemoApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CustomerController {
 
@@ -15,7 +17,7 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/")
-    Customer getCustomer(){
+    List<Customer> getCustomer(){
         return customerService.getCustomer();
 
     }
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,12 +1,23 @@
 package com.example.demo.customer;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CustomerService {
 
-    Customer getCustomer(){
-        return new Customer(1L, "Black Panther");
+    private final CustomerRepo customerRepo;
+
+    public CustomerService(
+            @Qualifier("fake") CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    List<Customer> getCustomer(){
+        return customerRepo.getCustomer();
     }
 }
