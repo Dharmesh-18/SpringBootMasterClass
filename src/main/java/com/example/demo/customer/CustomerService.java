093b6725b07badf0bbf1c46292db0a1c684fb071
WARBREAKER <dharmeshtiwari1280@gmail.com>
2023-06-05 07:21:28 +0530

    Logging using sl4J Logger

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -2,12 +2,16 @@ package com.example.demo.customer;
 
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
 
@@ -17,12 +21,18 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers(){
+        LOGGER.info("getCustomer was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id){
         return customerRepository
                 .findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id : " + id + " not found!"));
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException("customer with id : " + id + " not found!");
+                            LOGGER.error("error in getting Customer: {}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
