61817eaaedc81cf88d9c3883e387791d9ec06806
WARBREAKER <dharmeshtiwari1280@gmail.com>
2023-06-06 07:00:43 +0530

    @Value and Environment to access values from application.properties, @ConfigurationProperties to make class from key-value pairs of application.properties, spring profiles, properties<->yaml

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 
 @Configuration
@@ -12,11 +15,20 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
 
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
         return args -> {
             System.out.println("Welcome Dharmesh!");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.name"));
+            System.out.println(infoApp);
         };
     }
 
